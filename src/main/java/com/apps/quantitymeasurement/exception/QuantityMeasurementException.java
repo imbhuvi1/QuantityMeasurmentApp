3dd1fa0cc88990330df1a0cf235e86e8da0a9629
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-04-17 15:41:56 +0530

    [Bhuvnesh Singh Bhadauriya] Feat. Removed the files that were not needed

--- src/main/java/com/apps/quantitymeasurement/exception/QuantityMeasurementException.java
+++
@@ -1,7 +0,0 @@
-package com.apps.quantitymeasurement.exception;
-
-public class QuantityMeasurementException extends Exception{
-    public QuantityMeasurementException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/apps/quantitymeasurement/unit/IMeasurableUnit.java
+++
@@ -1,8 +0,0 @@
-package com.apps.quantitymeasurement.unit;
-
-
-public interface IMeasurableUnit {
-    public double getConversionFactor();
-    public String getUnitName();
-    public String getMeasurementType();
-}
